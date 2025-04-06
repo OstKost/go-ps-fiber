c1867a30662916d71617cf384de91fad6579a3fd
Ostrovskiy Konstantin <dev@ostkost.ru>
2025-04-06 15:09:13 +0300

    5-html-template

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html/v2"
 	"log"
 	"ostkost/go-ps-fiber/internal/home"
 	"ostkost/go-ps-fiber/pkg/config"
@@ -12,12 +14,16 @@ import (
 
 func main() {
 	config.Init()
-	app := fiber.New()
 	logConfig := config.NewLogConfig()
 	dbConfig := config.NewDatabaseConfig()
 	customLogger := logger.NewLogger(logConfig)
-	log.Println(dbConfig)
+	engine := html.New("./html", ".html")
 
+	fmt.Println(dbConfig)
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
 	}))
--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,6 +1,9 @@
 package home
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"strings"
+)
 
 type HomeHandler struct {
 	router fiber.Router
@@ -13,7 +16,42 @@ func NewHandler(router fiber.Router) {
 	h.router.Get("/", h.home)
 }
 
+type User struct {
+	Name string
+	Age  int
+}
+
 func (h HomeHandler) home(ctx *fiber.Ctx) error {
-	ctx.Hostname()
-	return ctx.SendString("OK")
+	//ctx.Hostname()
+	//tmpl := template.Must(template.ParseFiles("./html/page.html"))
+	//data := struct {
+	//	Count int
+	//	Name  string
+	//}{100, "foo"}
+	//var tpl bytes.Buffer
+	//if err := tmpl.Execute(&tpl, data); err != nil {
+	//	return fiber.NewError(fiber.StatusInternalServerError, "Template compile error")
+	//}
+	//ctx.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
+	//return ctx.Send(tpl.Bytes())
+
+	words := []string{"foo", "bar", "baz"}
+	users := []User{
+		{Name: "Ivan", Age: 10},
+		{Name: "Vasya", Age: 20},
+		{Name: "Kostya", Age: 30},
+	}
+
+	data := fiber.Map{
+		"Count":   100,
+		"Name":    "foo",
+		"IsAdmin": false,
+		"Users":   users,
+		"Words":   words,
+		"ToUpper": func(s string) string {
+			return strings.ToUpper(s)
+		},
+	}
+
+	return ctx.Render("page", data)
 }
