5a4f74fbfb3d878e0743387e0af031f351f1ef8e
Ostrovskiy Konstantin <dev@ostkost.ru>
2025-06-13 00:43:02 +0300

    fix-6

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -7,23 +7,25 @@ import (
 
 func AuthMiddleware(store *session.Store) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		sess, err := store.Get(ctx)
-		if err != nil {
-			ctx.Status(500).SendString("Ошибка получения сессии")
-			return ctx.Next()
-		}
 		userId := 0
 		userName := ""
 		userEmail := ""
-		if id, ok := sess.Get("userId").(int); ok {
-			userId = id
+
+		sess, err := store.Get(ctx)
+		if err != nil {
+			ctx.Status(500).SendString("Ошибка получения сессии")
+		} else {
+			if id, ok := sess.Get("userId").(int); ok {
+				userId = id
+			}
+			if name, ok := sess.Get("name").(string); ok {
+				userName = name
+			}
+			if email, ok := sess.Get("email").(string); ok {
+				userEmail = email
+			}
 		}
-		if name, ok := sess.Get("name").(string); ok {
-			userName = name
-		}
-		if email, ok := sess.Get("email").(string); ok {
-			userEmail = email
-		}
+
 		// Сохраняем данные в Locals
 		ctx.Locals("userId", userId)
 		ctx.Locals("userEmail", userEmail)
